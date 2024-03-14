545999faba65e5dcc2120240d75d5439c05754da
Artem <yakovenko.art1@gmail.com>
2024-03-14 14:28:05 +0500

    random added

--- frontend/src/components/BookForm/BookForm.jsx
+++ frontend/src/components/BookForm/BookForm.jsx
@@ -1,25 +1,49 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
 import { addBook } from "../../redux/books/actionCreators";
+import booksData from "../../data/books.json";
 import "./BookForm.css";
 
 export default function BookForm() {
     const [title, setTitle] = useState("");
     const [author, setAuthor] = useState("");
+    const books = useSelector((state) => state.books);
     const dispatch = useDispatch();
+
+    const findMatch = (arr, obj) => {
+        return arr.find(
+            (book) => book.author === obj.author && book.title === obj.title
+        );
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (title.trim() && author.trim()) {
+        if (title && author && !findMatch(books, { title, author })) {
             const book = {
                 title,
                 author,
+                id: uuidv4(),
             };
             dispatch(addBook(book));
             setTitle("");
             setAuthor("");
         }
     };
+    const handleAddRandomBook = () => {
+        const randomIndex = Math.floor(Math.random() * booksData.length);
+        const randomBook = booksData[randomIndex];
+        const book = {
+            title: randomBook.title,
+            author: randomBook.author,
+            id: uuidv4(),
+        };
+        if (findMatch(books, book)) {
+            return handleAddRandomBook();
+        }
+        dispatch(addBook(book));
+    };
+
     return (
         <section className="app-block book-form">
             <h2>Add a new book</h2>
@@ -30,7 +54,7 @@ export default function BookForm() {
                         type="text"
                         id="title"
                         value={title}
-                        onChange={(e) => setTitle(e.target.value)}
+                        onChange={(e) => setTitle(e.target.value.trim())}
                     ></input>
                 </div>
                 <div>
@@ -39,10 +63,13 @@ export default function BookForm() {
                         type="text"
                         id="author"
                         value={author}
-                        onChange={(e) => setAuthor(e.target.value)}
+                        onChange={(e) => setAuthor(e.target.value.trim())}
                     ></input>
                 </div>
                 <button type="submit">Add book</button>
+                <button type="button" onClick={handleAddRandomBook}>
+                    Add random
+                </button>
             </form>
         </section>
     );
--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -1,8 +1,13 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { deleteBook } from "../../redux/books/actionCreators";
 import "./BookList.css";
 
 export default function BookList() {
     const books = useSelector((state) => state.books);
+    const dispatch = useDispatch();
+    const handleDeleteBook = (id) => {
+        dispatch(deleteBook(id));
+    };
 
     return (
         <section className="app-block book-list">
@@ -10,10 +15,17 @@ export default function BookList() {
             {books.length ? (
                 <ul>
                     {books.map((book, i) => (
-                        <li key={i}>
+                        <li key={book.id}>
                             <div className="book-info">
-                                {++i}. '{book.title}' <i>by</i>{" "}
+                                {++i}. {book.title} <i>by</i>{" "}
                                 <strong>{book.author}</strong>
+                            </div>
+                            <div className="book-actions">
+                                <button
+                                    onClick={() => handleDeleteBook(book.id)}
+                                >
+                                    Delete
+                                </button>
                             </div>
                         </li>
                     ))}
